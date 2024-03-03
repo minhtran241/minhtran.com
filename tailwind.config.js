ca1c9a5f37659711976ec566405a50f129d6063c
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-03-03 16:36:04 -0500

    reconfig tailwind container

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,19 +24,13 @@ module.exports = {
     theme: {
         container: {
             center: true,
-            padding: '6rem',
+            padding: '2rem',
             screens: {
-                '2xl': { max: '1535px' },
-                xl: { max: '1279px' },
-                lg: { max: '1023px' },
-                md: { max: '767px' },
-                sm: { max: '639px' },
-
-                minsm: { min: '640px' },
-                minmd: { min: '768px' },
-                minlg: { min: '1024px' },
-                minxl: { min: '1280px' },
-                min2xl: { min: '1536px' },
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1536px',
             },
         },
         extend: {
