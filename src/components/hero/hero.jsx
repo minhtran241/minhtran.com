40dab3caed1030a19aca02bdbdd1f63e3b61853c
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-03-14 21:41:46 -0400

    update ui of hero section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,24 +1,32 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Suspense } from 'react';
 import Loading from '@/app/loading';
+import { useTheme } from 'next-themes';
 
 const HeroComponent = () => {
+    const { theme } = useTheme();
     return (
         <div className="">
             <div className="shadow-lg transform duration-300 easy-in-out">
-                <div className="h-40 overflow-hidden">
+                <div className="overflow-hidden lg:h-28 md:h-24 h-20">
                     <Image
                         className="rounded-md w-full object-cover"
-                        src="/bg-image.jpeg"
-                        alt="background_image"
+                        src={
+                            theme === 'dark'
+                                ? 'https://9to5mac.com/wp-content/uploads/sites/6/2021/04/hello-Blue-2-dragged.jpg?quality=82&strip=all'
+                                : 'https://9to5mac.com/wp-content/uploads/sites/6/2021/04/hello-Blue-1-dragged.jpg?quality=82&strip=all'
+                        }
+                        alt="bg-image"
                         width={1600}
                         height={400}
                     />
                 </div>
                 <div className="flex justify-center px-5 -mt-12">
-                    <Avatar className="h-32 w-32 bg-white dark:bg-black p-2">
+                    <Avatar className="bg-white dark:bg-black p-2 lg:h-32 lg:w-32 md:h-28 md:w-28 h-24 w-24">
                         <AvatarImage src="/minhtran-ava.png" alt="minhtran" />
                         <AvatarFallback>MT</AvatarFallback>
                     </Avatar>
@@ -33,17 +41,17 @@ const HeroComponent = () => {
                         </p>
                         <div className="flex flex-col items-center gap-1 mt-2">
                             <Link
-                                href={process.env.CURRENT_ORG_LINK}
+                                href="https://www.gvsu.edu/"
                                 className="text-[#0033A0] dark:text-blue-600 font-semibold"
                             >
-                                {process.env.CURRENT_ORG}
+                                Grand Valley State University
                             </Link>
                             <p className="text-gray-600 dark:text-gray-400">
-                                {process.env.CURRENT_ROLE}
+                                B.S in Computer Science
                             </p>
                         </div>
                     </div>
-                    <p className="text-center pt-4 px-4">
+                    <p className="text-center pt-4 px-4 text-base">
                         Experienced Software and Data Engineer with 2 years in
                         backend systems. Specializing in API development,
                         performance optimization, and system design.
@@ -60,7 +68,7 @@ const HeroComponent = () => {
                         <Link
                             target="_blank"
                             className="text-center w-1/2 py-2 px-4 uppercase hover:bg-[#0033A0] dark:hover:bg-blue-600 hover:text-white font-medium transition"
-                            href={process.env.RESUME_LINK}
+                            href="/minhtran-resume.pdf"
                         >
                             Resume
                         </Link>
