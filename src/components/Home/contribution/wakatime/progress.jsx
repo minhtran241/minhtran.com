38e9aeeb269ceb76b2479e496da9174eac0ab032
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-06-17 17:10:28 +0700

    restructure the codebase of contribution stats section

--- src/components/Home/contribution/wakatime/progress.jsx
+++ src/components/Home/contribution/wakatime/progress.jsx
@@ -1,16 +1,16 @@
 import { cn } from '@/common/libs/cn';
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const Progress = ({ data, className }) => {
     const { name, percent = 0 } = data;
 
-    // const progressVariants = {
-    //     initial: { width: 0 },
-    //     animate: {
-    //         width: `${percent}%`,
-    //         transition: { delay: 0.8 },
-    //     },
-    // };
+    const progressVariants = {
+        initial: { width: 0 },
+        animate: {
+            width: `${percent}%`,
+            transition: { delay: 0.8 },
+        },
+    };
 
     return (
         <div className="flex items-center justify-between gap-3">
@@ -18,14 +18,14 @@ const Progress = ({ data, className }) => {
                 {name}
             </div>
             <div className="relative flex h-2 flex-1 justify-center rounded-full bg-neutral-200 dark:bg-gray-800">
-                <div
+                {/* <div
                     className={cn(
                         className,
                         'absolute left-0 top-0 h-2 rounded-full px-3'
                     )}
                     style={{ width: `${percent}%` }}
-                />
-                {/* <motion.span
+                /> */}
+                <motion.span
                     initial="initial"
                     animate="animate"
                     variants={progressVariants}
@@ -35,7 +35,7 @@ const Progress = ({ data, className }) => {
                     )}
                 >
                     &ensp;
-                </motion.span> */}
+                </motion.span>
             </div>
             <div className="w-8 text-right text-sm text-gray-600 dark:text-gray-400">
                 {percent.toFixed(0)}%
