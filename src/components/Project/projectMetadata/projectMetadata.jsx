aa3a0c96e3a8247c1b36ef6698f21dac473708b6
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-06-20 14:48:41 +0700

    add placeholder image

--- src/components/Project/projectMetadata/projectMetadata.jsx
+++ src/components/Project/projectMetadata/projectMetadata.jsx
@@ -17,14 +17,14 @@ const ProjectMetadata = ({ project }) => {
             </div>
             <div className="flex flex-col gap-2 mb-5">
                 {project.repo_link && (
-                    <div className="flex items-center gap-2 hover:text-[#0033A0] dark:hover:text-blue-600">
-                        <Github className="h-4 w-4" />
+                    <div className="flex items-center gap-2 hover:text-[#0033A0] dark:hover:text-blue-600 cursor-pointer font-semibold transition">
+                        <Github className="h-5 w-5" />
                         <Link href={project.repo_link}>Code repository</Link>
                     </div>
                 )}
                 {project.link && (
-                    <div className="flex items-center gap-2 hover:text-[#0033A0] dark:hover:text-blue-600">
-                        <Radio className="h-4 w-4" />
+                    <div className="flex items-center gap-2 hover:text-[#0033A0] dark:hover:text-blue-600 cursor-pointer font-semibold transition">
+                        <Radio className="h-5 w-5" />
                         <Link href={project.link}>
                             Live product or documentation
                         </Link>
