b15baf2af0566f15aa7ea773615d82f3c4dafcd2
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-06-23 10:54:08 +0700

    update styles of blog card

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -151,6 +151,7 @@ const BlogPage = async () => {
                         </div> */}
                     </div>
                 </div>
+                <Breakline />
                 <div className="flex flex-col gap-4">
                     {groupedPosts.map((category, index) => (
                         <div key={index}>
@@ -161,7 +162,7 @@ const BlogPage = async () => {
                                         {category.name}
                                     </h1>
                                 </div>
-                                <div className="grid grid-cols-1 gap-x-6 gap-y-6 md:grid-cols-2 md:gap-x-4 lg:gap-x-6 xl:grid-cols-3">
+                                <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
                                     {category.posts.map((post, index) => (
                                         <PostCard key={index} post={post} />
                                     ))}
--- src/components/Post/postCard/postCard.jsx
+++ src/components/Post/postCard/postCard.jsx
@@ -7,27 +7,27 @@ const PostCard = ({ post }) => {
     const month = createdAt.toLocaleString('default', { month: 'long' });
     const date = new Date(post.created_at).getDate();
     return (
-        <div
-            className="wow fadeInUp relative overflow-hidden"
-            data-wow-delay=".1s"
-        >
-            <div className="overflow-hidden flex flex-col gap-2">
-                <Link href={`/blog/${post.slug}`}>
-                    <div className="relative w-full lg:h-56 md:h-52 sm:h-44 h-44">
-                        <Image
-                            className="border rounded-lg border-[#0033A0] "
-                            src={post.thumbnail}
-                            alt={post.title}
-                            fill
-                        />
-                        <div className="absolute right-0 top-0 mr-3 mt-3 flex h-[75px] w-[75px] flex-col items-center justify-center rounded-full px-4 text-sm transition duration-500 ease-in-out bg-[#0033A0] dark:bg-blue-600 text-white  border border-[#0033A0] dark:border-blue-600">
-                            <span className="font-bold">{date}</span>
-                            <small>{month}</small>
-                        </div>
+        // <div
+        //     className="wow fadeInUp relative overflow-hidden"
+        //     data-wow-delay=".1s"
+        // >
+        <div className="flex flex-col gap-2">
+            <Link href={`/blog/${post.slug}`}>
+                <div className="relative w-full lg:h-56 md:h-52 sm:h-44 h-44">
+                    <Image
+                        className="border rounded-lg border-[#0033A0] "
+                        src={post.thumbnail}
+                        alt={post.title}
+                        fill
+                    />
+                    <div className="absolute right-0 top-0 mr-3 mt-3 flex h-[75px] w-[75px] flex-col items-center justify-center rounded-full px-4 text-sm transition duration-500 ease-in-out bg-[#0033A0] dark:bg-blue-600 text-white  border border-[#0033A0] dark:border-blue-600">
+                        <span className="font-bold">{date}</span>
+                        <small>{month}</small>
                     </div>
-                </Link>
-                {/* Tags */}
-                {/* <div className="flex flex-wrap gap-2 mt-4">
+                </div>
+            </Link>
+            {/* Tags */}
+            {/* <div className="flex flex-wrap gap-2 mt-4">
                     {post.tags?.map((tag, index) => (
                         <div
                             key={index}
@@ -38,36 +38,36 @@ const PostCard = ({ post }) => {
                         </div>
                     ))}
                 </div> */}
-                <div className="gap-2">
-                    <Link
-                        href={`/blog/${post.slug}`}
-                        className="inline-block text-lg font-semibold transition hover:text-[#0033A0] dark:hover:text-blue-600"
-                    >
-                        {post.title}
-                    </Link>
-                    {/* <p className="text-md text-justify font-light italic text-gray-600 dark:text-gray-400">
+            {/* <div className=""> */}
+            <Link
+                href={`/blog/${post.slug}`}
+                className="inline-block text-lg font-semibold transition hover:text-[#0033A0] dark:hover:text-blue-600"
+            >
+                {post.title}
+            </Link>
+            {/* <p className="text-md text-justify font-light italic text-gray-600 dark:text-gray-400">
                         {post.description?.length > 150
                             ? `${post.description?.substring(0, 150)}...`
                             : post.description}
                     </p> */}
-                    <div
-                        className="tooltip !text-start cursor-pointer"
-                        data-tip={post.description}
-                    >
-                        <p className="text-gray-600 dark:text-gray-400 line-clamp-2">
-                            {post.description}
-                        </p>
-                        <Link
+            <div
+                className="tooltip !text-start cursor-pointer"
+                data-tip={post.description}
+            >
+                <p className="text-gray-600 dark:text-gray-400 line-clamp-3">
+                    {post.description}
+                </p>
+                {/* <Link
                             href={`/blog/${post.slug}`}
                             className="flex items-center justify-end gap-2 text-sm text-[#0033A0] dark:text-blue-600"
                         >
                             Read More
                             <ArrowRight className="h-4 w-4 inline-block" />
-                        </Link>
-                    </div>
-                </div>
+                        </Link> */}
             </div>
+            {/* </div> */}
         </div>
+        // </div>
     );
 };
 
