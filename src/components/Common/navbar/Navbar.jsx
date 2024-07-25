cbea210d5af0c3e8f28d6a3e21eb2a66e043b4b2
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-07-25 20:22:56 +0700

    update UI and themes

--- src/components/Common/navbar/Navbar.jsx
+++ src/components/Common/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import ThemeChanger from '@/common/elements/ThemeChanger';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-primary text-primary-content top-0 z-50 shadow-lg bg-opacity-90 fixed">
+        <div className="navbar bg-primary text-primary-content top-0 z-50 shadow-lg bg-opacity-90 fixed rounded-box">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div
